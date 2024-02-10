63b5ba7bdb73b64ef4ed6043f6b7d9e9f4e9f540
Bipasha Bipin <bb84832@uga.edu>
2024-02-10 12:39:44 -0500

    models

--- back-end/server.js
+++ back-end/server.js
@@ -5,3 +5,5 @@ app.get('/', (req, res) => res.send('Hello world!'));
 
 const port = process.env.PORT || 8082;
 app.listen(port, () => console.log(`Server running on port ${port}`));
+
+const dbURI = 'mongodb+srv://bbUser:<B2021ipasha$>@atlascluster.6oa0mfv.mongodb.net/?retryWrites=true&w=majority';

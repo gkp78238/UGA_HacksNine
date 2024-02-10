1bc9a74821cdad51e2fd5f9fd6a414887d6f0a38
londonho <avh95246@uga.edu>
2024-02-10 15:23:30 -0500

    Test

--- back-end/routes/authRoutes.js
+++ back-end/routes/authRoutes.js
@@ -5,7 +5,7 @@ const router = express.Router();
 const authController = require('../controllers/authController');
 
 // Route for user registration
-router.post('/register', authController.register);
+router.post('/api/register', authController.register);
 
 // Route for user login
 router.post('/login', authController.login);
